bf887aa60cf153d389f39d158f2127783fadfc49
ee106 <daisyforad@gmail.com>
2015-11-20 14:29:39 -0800

    new version

--- MZP/src/project/src/hand_over.py
+++ MZP/src/project/src/hand_over.py
@@ -8,8 +8,11 @@ from baxter_interface import gripper as baxter_gripper
 import tf
 from tf2_msgs.msg import TFMessage
 from geometry_msgs.msg import Transform, Vector3, Twist
+from sensor_msgs.msg import Range
 import exp_quat_func as eqf
 import numpy as np
+from scipy import linalg
+# import IR file
 
 listener = None
 
@@ -18,10 +21,18 @@ def quat_to_rbt(pos, rot):
     rbt = eqf.create_rbt(omega, theta, np.array([pos[0], pos[1], pos[2]]))
     return rbt
 
-def rel_rbt(a_t, a_r, b_t, b_r):
-    rbt_a = quat_to_rbt(a_t, a_r)
-    rbt_b = quat_to_rbt(b_t, b_r)
-    return eqf.compute_gab(rbt_a, rbt_b)
+def callback(message):
+    global mail_range
+    mail_range = message.range
+
+
+def IR_range():
+    range_msg  = rospy.Subscriber('/robot/range/left_hand_range/state',Range, callback)
+  
+
+
+
+
 
 def main():
     rospy.init_node('moveit_node')
@@ -32,23 +43,19 @@ def main():
         try:
             (trans_marker_cam, rot_marker_cam) = listener.lookupTransform('ar_marker_3','left_hand_camera', rospy.Time())
             (trans_cam_hand, rot_cam_hand) = listener.lookupTransform('left_hand_camera', 'left_hand', rospy.Time())
-            (trans_base_hand, rot_base_hand) = listener.lookupTransform('left_hand', 'left_gripper_base', rospy.Time())
-            (trans_grip_base, rot_grip_base) = listener.lookupTransform('left_gripper_base', 'left_gripper', rospy.Time())
-            (trans_init, rot_init) = listener.lookupTransform('base', 'left_gripper', rospy.Time())
-
+            (trans_hand_base, rot_hand_base) = listener.lookupTransform('left_hand', 'base', rospy.Time())
             break
         except:
             continue
 
-    # Find the transform between the AR tag and the left gripper
-    print('initial pose')
-    print(trans_init)
+    # Find the transform between the AR tag and the base
+    g_marker_cam = quat_to_rbt(trans_marker_cam, rot_marker_cam)
+    g_cam_hand = quat_to_rbt(trans_cam_hand, rot_cam_hand)
+    g_hand_base = quat_to_rbt(trans_hand_base, rot_hand_base)
 
-    trans_marker_hand = rel_rbt(trans_marker_cam,rot_marker_cam,trans_cam_hand,rot_cam_hand)
-    trans_grip_hand = rel_rbt(trans_base_hand,rot_base_hand,trans_grip_base,rot_grip_base)
-    trans_final = np.dot(trans_marker_hand, trans_grip_hand)
+    g_final = linalg.inv(np.dot(np.dot(g_marker_cam, g_cam_hand), g_hand_base))
     print('final')
-    print(trans_final)
+    print(g_final)
 
 
     #Initialize moveit_commander
@@ -75,25 +82,20 @@ def main():
     goal_1 = PoseStamped()
     goal_1.header.frame_id = "base"
 
-    start_point = PoseStamped()
-    start_point.header.frame_id = "base"
-
 
     #x, y, and z position
-    x_pos = "%.2f" % (trans_init[0] - trans_final[1,3])
-    y_pos = "%.2f" % (trans_init[1] - trans_final[0,3])
-    z_pos = "%.2f" % (trans_init[2] - trans_final[2,3])
+    x_pos = "%.3f" % (g_final[0,3])
+    y_pos = "%.3f" % (g_final[1,3])
+    z_pos = "%.3f" % (g_final[2,3])
 
-    print(trans_init)
-    print(trans_final)
     print(x_pos)
     print(y_pos)
-    print(z_pos)
+    print(float(z_pos)+0.05)
 
 
     goal_1.pose.position.x = float(x_pos)
     goal_1.pose.position.y = float(y_pos)
-    goal_1.pose.position.z = float(z_pos) + 0.05
+    goal_1.pose.position.z = float(z_pos) + 0.07
     
     #Orientation as a quaternion
     goal_1.pose.orientation.x = 0.0
@@ -101,16 +103,6 @@ def main():
     goal_1.pose.orientation.z = 0.0
     goal_1.pose.orientation.w = 0.0
 
-
-    start_point.pose.position.x = 0.6
-    start_point.pose.position.y = 0.684
-    start_point.pose.position.z = 0.188
-    
-    #Orientation as a quaternion
-    start_point.pose.orientation.x = 0.0
-    start_point.pose.orientation.y = -1.0
-    start_point.pose.orientation.z = 0.0
-    start_point.pose.orientation.w = 0.0
     #Set the goal state to the pose you just defined
     left_arm.set_pose_target(goal_1)
 
@@ -124,12 +116,39 @@ def main():
     raw_input('Press <Enter> to move the left arm to goal pose 1 (path constraints are never enforced during this motion): ')
     left_arm.execute(left_plan)
 
-    # #Close the Gripper
-    # raw_input('Press <Enter> to close the gripper')
+    
+    rospy.Subscriber('/robot/range/left_hand_range/state',Range, callback)
+    rospy.sleep(1)
+    print(mail_range)
 
-    # left_gripper.close(timeout=60)
-    # print('Gripper closed')
+    #Check using IR to see of the mail is close enough
+    incre = 0.01
+    while mail_range > 0.049 and incre < 0.05:
+        print('Too far from the mail. Trying again...')
+        rospy.Subscriber('/robot/range/left_hand_range/state',Range, callback)
+        rospy.sleep(1)
+        print(mail_range)
+        goal_1.pose.position.z = float(z_pos) + 0.07 - incre
+        incre += 0.01
+            #Set the goal state to the pose you just defined
+        left_arm.set_pose_target(goal_1)
 
+        #Set the start state for the left arm
+        left_arm.set_start_state_to_current_state()
+        #Plan a path
+        left_plan = left_arm.plan()
+ 
+        #Execute the plan
+        raw_input('Press <Enter> to move the left arm to goal pose 1 (path constraints are never enforced during this motion): ')
+        left_arm.execute(left_plan)
+    print(mail_range)
+    #Close the Gripper
+    raw_input('Press <Enter> to close the gripper')
+
+    left_gripper.close(timeout=60)
+    print('Gripper closed')
+        
+        
 
     #Second goal pose -----------------------------------------------------
     rospy.sleep(2.0)
@@ -175,10 +194,10 @@ def main():
     left_arm.execute(left_plan)
 
 
-    # #Release the Gripper
-    # raw_input('Press <Enter> to open the gripper')
-    # left_gripper.open(block=True)
-    # print('Gripper opened')
+    #Release the Gripper
+    raw_input('Press <Enter> to open the gripper')
+    left_gripper.open(block=True)
+    print('Gripper opened')
 
 
 if __name__ == '__main__':
